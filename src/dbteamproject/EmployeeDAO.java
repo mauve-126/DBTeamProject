8736d49d67707f5a04a1273d363e4d5d1631c394
yyh <yyhyjh99@naver.com>
2020-11-08 21:15:27 +0900

    수정 삭제 하나씩만 되는 오류 있음 수정할 예정입니다

--- src/dbteamproject/EmployeeDAO.java
+++ src/dbteamproject/EmployeeDAO.java
@@ -12,15 +12,115 @@ public class EmployeeDAO {
 	private static String dbUser = "root";
 	private static String dbpasswd = "tracer85x1005@";
 	
-	void insert() {
+	public ArrayList<String> department() {
+		ArrayList<String> department = new ArrayList<String>();
+		department.add("all");
+		try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //JDBC 드라이버 연결
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+		Connection con = null;       //연결
+        PreparedStatement ps = null; //명령
+        ResultSet rs = null;         //결과
+        
+
+        try {
+           
+			con = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+            String sql = "select * from DEPARTMENT";
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while(rs.next()){
+            	String Dname = rs.getString(1);
+            	department.add(Dname);
+            	        
+            }//while
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+		
+		return department;
 		
 	}
-	void update() {
+	
+public Vector getList(ArrayList<String> attributes, ArrayList<String> tables){
 		
-	}
-	void delete() {
-		
-	}
+		try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //JDBC 드라이버 연결
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+	       
+        Vector data = new Vector();  //Jtable에 값을 쉽게 넣는 방법 1. 2차원배열   2. Vector 에 vector추가
+       
+        Connection con = null;       //연결
+        PreparedStatement ps = null; //명령
+        ResultSet rs = null;         //결과
+       
+        try{
+        	
+        	
+			con = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+//            String sql = "select * from DEPARTMENT d, EMPLOYEE e left outer join EMPLOYEE s on s.ssn = e.super_ssn  where e.dno = d.dnumber";
+//            ps = con.prepareStatement(sql);
+//            rs = ps.executeQuery();
+            
+            String all ="select * from DEPARTMENT d, EMPLOYEE e left outer join EMPLOYEE s on s.ssn = e.super_ssn  where e.dno = d.dnumber";
+			String department="select * from DEPARTMENT d, EMPLOYEE e left outer join EMPLOYEE s on s.ssn = e.super_ssn  where e.dno = d.dnumber and d.dname = ?";
+			String sql;
+
+			
+			if (tables.get(0) == null) {
+				sql = all;
+			}else {
+				sql = department;
+			}
+			ps = con.prepareStatement(sql);
+			if(sql == department) {
+				ps.setString(1, tables.get(0));
+			}
+			rs=ps.executeQuery();
+            
+           
+           
+            while(rs.next()){
+            	Vector row = new Vector();
+            	for (int i = 0; i< attributes.size(); i++) {
+            		if (attributes.get(i) == "NAME") {
+            			String fnameString;
+            			fnameString=rs.getString("e.fname");
+            			String minitString;
+            			minitString=rs.getString("e.minit");
+            			String lnameString;
+            			lnameString=rs.getString("e.lname");
+            			String name = fnameString + ", " + minitString +", "+lnameString;
+            			row.add(name);
+            		}else if(attributes.get(i) == "SUPERVISOR"){
+            			String fnameString;
+            			fnameString=rs.getString("s.fname");
+            			String minitString;
+            			minitString=rs.getString("s.minit");
+            			String lnameString;
+            			lnameString=rs.getString("s.lname");
+            			String name = fnameString + ", " + minitString +", "+lnameString;
+            			row.add(name);
+            		}else {
+            			String d=rs.getString(attributes.get(i));
+                		row.add(d);
+            			
+            		}
+            		  
+            		}
+            	data.add(row);
+            	
+            }//while
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return data;
+    }
 	public Vector getMemberList(){
 		
 		try {
@@ -52,6 +152,8 @@ public class EmployeeDAO {
 			num.add("d.dname");//12
            
 			con = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+			
+			
             String sql = "select * from DEPARTMENT d, EMPLOYEE e left outer join EMPLOYEE s on s.ssn = e.super_ssn  where e.dno = d.dnumber";
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
@@ -71,19 +173,18 @@ public class EmployeeDAO {
 				String superMinit = rs.getString(num.get(9));
 				String superLname = rs.getString(num.get(10));
 				String dnoString = rs.getString(num.get(11));
+				
+				String name = fnameString + ", " + minitString +", "+lnameString;
+				String sname = superFname + ", " + superMinit +", "+superLname;
                
                 Vector row = new Vector();
-                row.add(fnameString);
-                row.add(minitString);
-                row.add(lnameString);
+                row.add(name);
                 row.add(ssnString);
                 row.add(bdateString);
                 row.add(addressString);
                 row.add(sexString);
                 row.add(salaryString);
-                row.add(superFname);
-                row.add(superMinit);
-                row.add(superLname);
+                row.add(sname);
                 row.add(dnoString);
                
                 data.add(row);             
@@ -93,142 +194,66 @@ public class EmployeeDAO {
         }
         return data;
     }
-
-	public ArrayList<EmployeeDTO> search(ArrayList<String> attributes, ArrayList<String> tables) {
-		Connection conn = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+	public void Delete(String Ssn) {
 		
-		EmployeeDTO em = null;
-		ArrayList<EmployeeDTO> selectedResultArrayList =  new ArrayList<EmployeeDTO>();
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
-//			int wantedArrayListLength = wanted.size();
-			String all ="select * from DEPARTMENT d, EMPLOYEE e left outer join EMPLOYEE s on s.ssn = e.super_ssn  where e.dno = d.dnumber";
-			String department="select * from DEPARTMENT d, EMPLOYEE e left outer join EMPLOYEE s on s.ssn = e.super_ssn  where e.dno = d.dnumber and d.dname = ?";
-			String sql;
-//			for(int i=0;i<wantedArrayListLength;i++) {
-//				sql += wanted.get(i);
-//				if(i!=wantedArrayListLength-1) {
-//					sql+= ",";
-//				}
-//			}
-			//원하는 쿼리에 있는 항목들만 읽어옴
-			
-			if (tables.get(1) == null) {
-				sql = all;
-			}else {
-				sql = department;
-			}
-			ps = conn.prepareStatement(sql);
-			if(sql == department) {
-				ps.setString(1, tables.get(1));
-			}
-			rs=ps.executeQuery();
-			
-			while(rs.next()) {
-				String fnameString;
-				if(attributes.get(0) == null) {
-					fnameString = null;
-				}else {
-					fnameString=rs.getString(attributes.get(0));
-				}
-				String minitString;
-				if(attributes.get(1) == null) {
-					minitString = null;
-				}else {
-					minitString=rs.getString(attributes.get(1));
-				}
-				String lnameString;
-				if(attributes.get(2) == null) {
-					lnameString = null;
-				}else {
-					lnameString=rs.getString(attributes.get(2));
-				}
-				String ssnString;
-				if(attributes.get(3) == null) {
-					ssnString = null;
-				}else {
-					ssnString=rs.getString(attributes.get(3));
-				}
-				String bdateString;
-				if(attributes.get(4) == null) {
-					bdateString = null;
-				}else {
-					bdateString=rs.getString(attributes.get(4));
-				}
-				String addressString;
-				if(attributes.get(5) == null) {
-					addressString = null;
-				}else {
-					addressString=rs.getString(attributes.get(5));
-				}
-				String sexString;
-				if(attributes.get(6) == null) {
-					sexString = null;
-				}else {
-					sexString=rs.getString(attributes.get(6));
-				}
-				String salaryString;
-				if(attributes.get(7) == null) {
-					salaryString = null;
-				}else {
-					salaryString=rs.getString(attributes.get(7));
-				}
-				String superFname;
-				if(attributes.get(8) == null) {
-					superFname = null;
-				}else {
-					superFname=rs.getString(attributes.get(8));
-				}
-				String superMinit;
-				if(attributes.get(9) == null) {
-					superMinit = null;
-				}else {
-					superMinit=rs.getString(attributes.get(9));
-				}
-				String superLname;
-				if(attributes.get(10) == null) {
-					superLname = null;
-				}else {
-					superLname=rs.getString(attributes.get(10));
-				}
-				String dnoString;
-				if(attributes.get(11) == null) {
-					dnoString = null;
-				}else {
-					dnoString=rs.getString(attributes.get(11));
-				}
-				em = new EmployeeDTO(fnameString, minitString, lnameString,ssnString, bdateString, addressString, sexString, salaryString, superFname, superMinit, superLname, dnoString);
-				selectedResultArrayList.add(em);
-			}
-		}catch(Exception e) {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //JDBC 드라이버 연결
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+       
+        Connection con = null;  
+        try {
+        	con = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+	    	  String del="delete from EMPLOYEE where ssn = ?";
+	    	  PreparedStatement p2=con.prepareStatement(del);
+	          p2.clearParameters();
+	          p2.setString(1, Ssn);
+	          int delr=p2.executeUpdate();
+		      if( delr == 0 ){
+		          System.out.println("삭제 할  내용을 찾을 수 없습니다.");
+		      }else{
+		          System.out.println("삭제 되었습니다.");
+		          List.ShowTable();
+		      }
+	    	  
+        } catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 		}
-		return selectedResultArrayList;
+	    
+	}
+public void Update(String Ssn, String Salary) {
+		System.out.println(Salary);
+		System.out.println(Ssn);
+		try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //JDBC 드라이버 연결
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+       
+        Connection con = null;  
+        try {
+        	con = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+            String update="update EMPLOYEE set salary = ? where ssn = ?";
+            PreparedStatement p3=con.prepareStatement(update);
+            p3.clearParameters();
+            p3.setString(1, Salary);
+            p3.setString(2, Ssn);
+            int up=p3.executeUpdate();
+            if( up > 0 ){
+                System.out.println("갱신");
+                List.ShowTable();
+            }else{
+                System.out.println("갱신 불가");
+            }
+
+ 
+        } catch (SQLException e) {
+			e.printStackTrace();
+		}
+	    
 	}
 
 
--- src/dbteamproject/List.java
+++ src/dbteamproject/List.java
@@ -1,144 +1,269 @@
 package dbteamproject;
 
 
-import java.awt.BorderLayout;
-import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
-public class List extends JFrame implements MouseListener,ActionListener{
-	 Vector v;  
-	    Vector cols;
-	    DefaultTableModel model;
-	    JTable jTable;
-	    JScrollPane pane;
-	    JPanel pbtn;
-	    JButton btnInsert;
-	       
-	   
-	    public List(){
-	        super("Information Retrieval System");
-	        //v=getMemberList();
-	        //MemberDAO
-	        EmployeeDAO dao = new EmployeeDAO();
-	        v = dao.getMemberList();
-	        System.out.println("v="+v);
-	        cols = getColumn();
-	       
-	        //public DefaultTableModel()
-	        //public DefaultTableModel(int rowCount, int columnCount)
-	        //public DefaultTableModel(Vector columnNames, int rowCount)
-	        //public DefaultTableModel(Object[] columnNames, int rowCount)
-	        //public DefaultTableModel(Vector data,Vector columnNames)
-	        //public DefaultTableModel(Object[][] data,Object[] columnNames)
-	       
-	        model = new DefaultTableModel(v, cols);
-	       
-	        //JTable()
-	        //JTable(int numRows, int numColumns)
-	        //JTable(Object[][] rowData, Object[] columnNames)
-	        //JTable(TableModel dm)
-	        //JTable(TableModel dm, TableColumnModel cm)
-	        //JTable(TableModel dm, TableColumnModel cm, ListSelectionModel sm)
-	        //JTable(Vector rowData, Vector columnNames)
-	       
-	        //jTable = new JTable(v,cols);
-	        jTable = new JTable(model);
-	        pane = new JScrollPane(jTable);
-	        add(pane);
-	       
-	        pbtn = new JPanel();
-	        btnInsert = new JButton("회원가입");
-	        pbtn.add(btnInsert);
-	        add(pbtn,BorderLayout.NORTH);
-	       
-	       
-	        jTable.addMouseListener(this); //리스너 등록
-	        btnInsert.addActionListener(this); //회원가입버튼 리스너 등록
-	       
-	        setSize(1000,500);
-	        setVisible(true);
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    }//end 생성자
-	   
-	   
-	    //JTable의 컬럼
-	    public Vector getColumn(){
-	        Vector col = new Vector();
-	        col.add("fname");
-	        col.add("minit");
-	        col.add("lname");
-	        col.add("ssn");
-	        col.add("bdate");
-	        col.add("address");
-	        col.add("sex");
-	        col.add("salary");
-	        col.add("sup");
-	        col.add("er");
-	        col.add("visor");
-	        col.add("department");
-	       
-	        return col;
-	    }//getColumn
-	   
-	   
-	    //Jtable 내용 갱신 메서드
-	    public void jTableRefresh(){
-	       
-	        EmployeeDAO dao = new EmployeeDAO();
-	        DefaultTableModel model= new DefaultTableModel(dao.getMemberList(), getColumn());
-	        jTable.setModel(model);    
-	       
-	    }
-	   
-	    public static void main(String[] args) {
-	        new List();
-	    }//main
-	    @Override
-	    public void mouseClicked(MouseEvent e) {
-	        // mouseClicked 만 사용
-	        int r = jTable.getSelectedRow();
-	        String id = (String) jTable.getValueAt(r, 0);
-	        //System.out.println("id="+id);
-	        //MemberProc mem = new MemberProc(id,this); //아이디를 인자로 수정창 생성
-	               
-	    }
-	    @Override
-	    public void mouseEntered(MouseEvent e) {
-	        // TODO Auto-generated method stub
-	       
-	    }
-	    @Override
-	    public void mouseExited(MouseEvent e) {
-	        // TODO Auto-generated method stub
-	       
-	    }
-	    @Override
-	    public void mousePressed(MouseEvent e) {
-	        // TODO Auto-generated method stub
-	       
-	    }
-	    @Override
-	    public void mouseReleased(MouseEvent e) {
-	        // TODO Auto-generated method stub
-	       
-	    }
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-	        //버튼을 클릭하면
-	        if(e.getSource() == btnInsert ){
-	            //new MemberProc(this);
-	           
-	            /*테스트*/
-	            //dao = new MemberDAO();           
-	            //dao.userSelectAll(model);
-	            //model.fireTableDataChanged();
-	            //jTable.updateUI();           
-	            //jTable.requestFocusInWindow();
-	           
-	        }
-	       
-	    }
+
+public class List extends JFrame{
+	
+    static Vector v;  
+    static Vector cols;
+	public static JLabel lbCount, lbSelected;
+	public static ArrayList<String> selected_ssn = new ArrayList<String>();
+	public static ArrayList<String> selectedname = new ArrayList<String>();
+	public static ArrayList<Integer> selected_row = new ArrayList<Integer>();
+
+	
+	private static Connection con;
+	
+	private static JComboBox cmbDepartment = new JComboBox();
+	private static JCheckBox chkBoxName = new JCheckBox("NAME", true);
+	private static JCheckBox chkBoxSsn = new JCheckBox("SSN", true);
+	private static JCheckBox chkBoxBdate = new JCheckBox("BDATE", true);
+	private static JCheckBox chkBoxAddress = new JCheckBox("ADDRESS", true);
+	private static JCheckBox chkBoxSex = new JCheckBox("SEX", true);
+	private static JCheckBox chkBoxSalary = new JCheckBox("SALARY", true);
+	private static JCheckBox chkBoxSuperName = new JCheckBox("SUPERVISOR", true);
+	private static JCheckBox chkBoxDname = new JCheckBox("DNAME", true);
+	private static JButton btnSearch = new JButton("검색");
+	
+	private static DefaultTableModel defaultTableModel;
+	private static JTable table;
+	private static JScrollPane sPane;
+	
+	static class MyDefaultTableCellRenderer extends DefaultTableCellRenderer {
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+			JCheckBox chkBox = new JCheckBox();
+			chkBox.setSelected(isSelected);
+			chkBox.setHorizontalAlignment(JCheckBox.CENTER);
+			
+		
+			if(chkBox.isSelected()) {
+				String selectedssn = (((Vector) v.get(row)).get(1)).toString();
+				String selected_name = (((Vector) v.get(row)).get(0)).toString();
+				if(selected_ssn.contains(selectedssn)) {
+					selected_ssn.remove(selected_name);
+				} else {
+					selected_ssn.add(selectedssn);
+					selectedname.add(selected_name);
+				}
+
+				lbSelected.setText("select : "+selectedname.toString());
+				System.out.println("selected_ssn :: " + selected_ssn.toString());
+			}
+			return chkBox;
+		}
+
+	}
+	
+	
+	public static void main(String[] args) throws SQLException {
+		
+		
+		// Frame 생성 및 세팅
+		JFrame mainFrame = new JFrame();
+		mainFrame.setTitle("Informational Retrieval System");
+		mainFrame.setSize(1200, 380);
+		mainFrame.setLayout(null);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// 상단 패널 생성
+		JPanel topPanel = new JPanel();
+		topPanel.setBounds(0, 0, 1200, 40);
+		EmployeeDAO Dao = new EmployeeDAO();
+		ArrayList<String> arrDepartment = new ArrayList<String>();
+		arrDepartment = Dao.department(); 
+		
+		cmbDepartment = new JComboBox(arrDepartment.toArray(new String[arrDepartment.size()]));
+		
+		topPanel.add(cmbDepartment);
+		topPanel.add(chkBoxName);
+		topPanel.add(chkBoxSsn);
+		topPanel.add(chkBoxBdate);
+		topPanel.add(chkBoxAddress);
+		topPanel.add(chkBoxSex);
+		topPanel.add(chkBoxSalary);
+		topPanel.add(chkBoxSuperName);
+		topPanel.add(chkBoxDname);
+		topPanel.add(btnSearch);
+		
+		mainFrame.add(topPanel);
+		
+		
+
+		
+		// 하단 패널 생성
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(null);
+		bottomPanel.setBounds(0, 260, 1200, 100);
+		
+		lbCount = new JLabel("Employee Count : ");
+		lbCount.setFont(new Font("Dialog", Font.BOLD, 12));
+		lbSelected = new JLabel("Selected Employee : ");
+		lbSelected.setFont(new Font("Dialog", Font.BOLD, 14));
+		JLabel lbSalary = new JLabel("new salary : ");
+		lbSalary.setFont(new Font("Dialog", Font.BOLD, 12));
+		JTextField tfSalary = new JTextField();
+		JButton btnUpdate = new JButton("UPDATE");
+		JButton btnDelete = new JButton("DELETE");
+		
+		bottomPanel.add(lbCount);
+		bottomPanel.add(lbSelected);
+		bottomPanel.add(lbSalary);
+		bottomPanel.add(tfSalary);
+		bottomPanel.add(btnUpdate);
+		bottomPanel.add(btnDelete);
+		
+		lbCount.setBounds(5, 5, 1200, 20);
+		lbSelected.setBounds(5, 25, 1200, 20);
+		lbSalary.setBounds(5, 50, 100, 20);
+		tfSalary.setBounds(90, 50, 100, 20);
+		btnUpdate.setBounds(200, 50, 100, 20);
+		btnDelete.setBounds(1050, 50, 100, 20);
+		
+		mainFrame.add(bottomPanel);
+		
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) { 
+				
+				for(int i = 0; i < selected_ssn.size(); i++) {
+					Dao.Delete(selected_ssn.get(i));
+					ShowTable();
+					mainFrame.add(sPane);
+					System.out.println(i);
+					}
+			}
+		});
+		
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) { 
+					if(!tfSalary.getText().isEmpty()) {
+						System.out.println(selected_ssn.size());
+						for(int i = 0; i < selected_ssn.size(); i++) {
+							System.out.println(selected_ssn.get(i));
+							Dao.Update(selected_ssn.get(i), tfSalary.getText());
+							ShowTable();
+							mainFrame.add(sPane);
+							System.out.println(i);
+							
+						}
+					}
+			}
+		});
+				
+		// 중앙 데이터뷰 생성		
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				ShowTable();
+				mainFrame.add(sPane);
+			}
+			
+		});		
+		
+		mainFrame.setVisible(true);
+	
+	}
+	public static void ShowTable() {
+		
+	     	
+		lbSelected.setText("select : ");
+		selectedname.clear();
+		selected_ssn.clear();
+		ArrayList<String> tableHeader = new ArrayList<String>();
+		ArrayList<String> tableFilter = new ArrayList<String>();
+		
+		if(chkBoxName.isSelected()) {
+			tableHeader.add(chkBoxName.getText());
+			tableFilter.add("E.Fname");
+			tableFilter.add("E.Minit");
+			tableFilter.add("E.Lname");
+		}
+		if(chkBoxSsn.isSelected()) {
+			tableHeader.add(chkBoxSsn.getText());
+			tableFilter.add("E.Ssn");
+		}
+		if(chkBoxBdate.isSelected()) {
+			tableHeader.add(chkBoxBdate.getText());	
+			tableFilter.add("E.Bdate");
+		}
+		if(chkBoxAddress.isSelected()) {
+			tableHeader.add(chkBoxAddress.getText());		
+			tableFilter.add("E.Address");
+		}
+		if(chkBoxSex.isSelected()) {
+			tableHeader.add(chkBoxSex.getText());		
+			tableFilter.add("E.Sex");
+		}
+		if(chkBoxSalary.isSelected()) {
+			tableHeader.add(chkBoxSalary.getText());	
+			tableFilter.add("E.Salary");
+		}
+		if(chkBoxSuperName.isSelected()) {
+			tableHeader.add(chkBoxSuperName.getText());		
+			tableFilter.add("S.Fname");
+			tableFilter.add("S.Minit");
+			tableFilter.add("S.Lname");
+		}
+		if(chkBoxDname.isSelected()) {
+			tableHeader.add(chkBoxDname.getText());		
+			tableFilter.add("Dname");
+		}
+		
+		Vector col = new Vector();
+		ArrayList<String> tables = new ArrayList<String>();
+		if(cmbDepartment.getSelectedIndex() != 0) {
+			tables.add(cmbDepartment.getSelectedItem().toString());
+		}else {
+			tables.add(null);
+		}
+		for (int i = 0; i<tableHeader.size(); i++) {
+  		  String head = tableHeader.get(i);
+      	  col.add(head);
+      	  }
+		col.add("select");
+		
+		EmployeeDAO dao = new EmployeeDAO();
+	     //v = dao.getMemberList();
+		v = dao.getList(tableHeader,tables);
+		
+		defaultTableModel = new DefaultTableModel(v, col);
+	     table = new JTable(defaultTableModel);
+	     sPane = new JScrollPane(table);
+	     sPane.setBounds(0, 50, 1200, 200);
+		
+	     System.out.println("v="+v);
+	     //cols = getColumn();
+	     DefaultTableCellRenderer renderer = new MyDefaultTableCellRenderer();
+			table.getColumn("select").setCellRenderer(renderer);
+			table.getColumn("select").setPreferredWidth(15);
+		
+	}
 
 }
